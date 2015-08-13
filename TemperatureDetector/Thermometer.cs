b1e001741606006715b5e66090243feb14fc8e42
Denis Tran <denis.tran@gmail.com>
2015-08-12 20:39:55 -0700

    first commit, not much code yet

---
+++ TemperatureDetector/Thermometer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureDetector
+{
+    class Thermometer
+    {
+        // member variables
+        
+        // enums
+        enum Units { Celsius, Farenheit };
+
+        // methods
+
+        /// function that takes a list of temperatures and outputs a list of times where a temperature was detected
+        /// Parameters:
+        public List<int> DetectTemperatures()
+        {
+
+        }
+    }
+}
